ea5684f791a50f4fe958104a38617040ea5c6172
Villalobos <jmvillalobos87@gmail.com>
2025-08-03 13:25:00 -0600

    19V

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -85,10 +85,10 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,escuela-manejo.o
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
         conn_max_age=600,
-        conn_health_checks=True,
+        ssl_require=True  # puedes quitar esto si no usas SSL
     )
 }
 
